ea9a934a2a1d6a57fa10cbc456074ad43586ed7b
dmoneybags <86892271+dmoneybags@users.noreply.github.com>
2021-11-17 16:04:36 -0800

    Initial Commit

---
+++ apollo4/apollo4App.swift
@@ -0,0 +1,17 @@
+//
+//  apollo4App.swift
+//  apollo4
+//
+//  Created by Daniel DeMoney on 11/17/21.
+//
+
+import SwiftUI
+
+@main
+struct apollo4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
